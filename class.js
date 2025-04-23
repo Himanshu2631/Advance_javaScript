4d5b401e0878d8e1cd7166dd395e2d2b7bc7d875
Himanshu2631 <himanshusengar235@gmail.com>
2025-04-23 11:01:03 +0530

    Add files via upload

--- class.js
+++ class.js
@@ -80,4 +80,147 @@
   
 //   let msg = person.compliment("Bob", "jacket");
 //   console.log(msg);  
-  // Output: That's a wonderful jacket, Bob!
+/*
+class Person {
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+
+  greet() {
+    console.log("Hi there! I'm", this.firstname);
+  }
+}
+
+let p = new Person("Maaike", "van Putten");
+p.greet();
+*/
+//Properties:- Properties are sometimes called fields, hold the data of the class.
+
+// class Person {
+//   constructor(firstname, lastname) {
+//     this.firstname = firstname;
+//     this.lastname = lastname;
+//   }
+// }
+
+// let p = new Person("Maaike", "van Putten");
+// console.log("Hi", p.firstname);
+
+// class Person {
+//   #firstname
+//   #lastname
+//   constructor(firstname, lastname) {
+//      this.#firstname = firstname;
+//      this.#lastname = lastname;
+//     }
+// }
+//  let p = new Person("Maaike", "van Putten");
+//  console.log(p.firstname);
+
+// class Person {
+//   constructor(firstname, lastname){
+//     if(firstname.startsWith("M")){
+//       this.#firstname = forstname;
+//     }else {
+//       this.#firstname = "M" + firstname;
+//     }
+//     this.#lastname = lastname;
+//   }
+// }
+
+// let p = new Person("kay", "Moon");
+
+// class Person {
+//   #firstname;
+//   #lastname;
+//   constructor(firstname, lastname) {
+//      this.#firstname = firstname;
+//      this.#lastname = lastname;
+//     }
+
+//   get firstname() {
+//      return this.#firstname;
+//   }
+//   set firstname(firstname) {
+//     this.#firstname = firstname;
+//   }
+//   get lastname() {
+//     return this.#lastname;
+//   }
+//   set lastname(lastname) {
+//    this.#lastname = lastname;
+//   }
+// }
+// let p = new Person("Maria","Saga");
+// console.log(p.firstname);
+
+//Inheritance
+
+// class Vehicle {
+//   constructor(color, currentSpeed, maxSpeed){
+//     this.color = color;
+//     this.currentSpeed = currentSpeed;
+//     this.maxSpeed = maxSpeed;
+//   }
+//   move() {
+//     console.log("moving at", this.currentSpeed);
+//   }
+//   accelerate(amount) {
+//     this.currentSpeed += amount;
+//   }
+// }
+
+
+// class Vehicle{
+//   constructor(color, currentSpeed,maxspeed){
+//       this.color = color;
+//       this.currentSpeed = currentSpeed;
+//       this.maxspeed = maxspeed;
+//   }
+//   move(){
+//       console.log("Moving at",this.currentSpeed);    // here use this : talking about a perticuler class.
+//   }
+//   accelerate(amount){
+//       this.currentSpeed += amount;
+//   }
+// }
+// class Motorcycle extends Vehicle{
+
+//   constructor(color,currentSpeed,maxspeed,fuel){
+//       super(color,currentSpeed,maxspeed);
+//       this.fuel = fuel;
+//   }
+//   doWheelie(){
+//       console.log("Driving on one wheel!");
+//   }
+// }
+// let Motor = new Motorcycle("Black", 0, 250,"Gasoline");
+// console.log(Motor.color);
+// console.log(Motor.currentSpeed);
+// console.log(Motor.maxspeed);
+// console.log(Motor.fuel);
+// Motor.accelerate(50);
+// Motor.move();
+//We cannot access any Motorcycle specific properties or methods in our Vehicle class.
+
+//Prototypes
+
+// class Person{
+//   constructor(firstname,lastname){
+//       this.firstname=firstname;
+//       this.lastname=lastname;
+//   }
+//   greet(){
+//       console.log("hi there");
+//   }
+// }
+// Person.prototype.introduce=function(){
+//   console.log("Hi,I'm",this.firstname);
+// };
+// Person.prototype.favoriteColor="green";
+// let p=new Person("Maria","Saga");
+// console.log(p.favoriteColor);
+// p.introduce();
+
+
